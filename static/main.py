1b2de388c68b356d1142e46963874bc1d9579934
Kanav Miglani <108290801+itscyfix@users.noreply.github.com>
2022-07-02 01:00:57 +0530

    Add files via upload

---
+++ static/main.py
@@ -0,0 +1,10 @@
+from email import message
+from socket import timeout
+from turtle import title
+from plyer import notification
+import time
+
+if __name__ == "__main__":
+    while True:
+        notification.notify(title = "Drink water " , message = "drink water its another 15 minutes" , timeout = 5 )
+        time.sleep(900)
